5f79592bc2c122bc7b597edabeb7b1f4b673d38d
hanbosun <sunhanbo@gmail.com>
2016-07-20 22:56:09 -0500

    Fix the image processing
    Fix the image processing

--- src/main/java/org/hanbo/mvc/utilities/ImageFileProcessingUtils.java
+++ src/main/java/org/hanbo/mvc/utilities/ImageFileProcessingUtils.java
@@ -2,35 +2,35 @@ package org.hanbo.mvc.utilities;
 
 public class ImageFileProcessingUtils
 {
-   private String externalProgramPath;
+   private String externalProgramsPath;
    
-   public ImageFileProcessingUtils(String extProgPath)
+   public ImageFileProcessingUtils(String extProgsPath)
    {
-      externalProgramPath = extProgPath;
+      externalProgramsPath = extProgsPath;
    }
    
-   public void resizeImage(String originalImgFile,
+   public boolean resizeImage(String originalImgFile,
       String destImgFile,
       boolean center,
       boolean transparentBg,
       int destWidth,
       int destHeight)
    {
+      String exeProgPath = getProgramPath(externalProgramsPath, "convert");
       String destImageDimension
          = String.format("%dx%d", destWidth, destHeight);
       
       ProgramInvoker invoker
          = new ProgramInvoker();
-      invoker.setCommandExec(externalProgramPath);
+      invoker.setCommandExec(exeProgPath);
+      invoker.getCmdArguments().add("-trim");
       invoker.getCmdArguments().add(originalImgFile);
-      invoker.getCmdArguments().add("-size");
-      invoker.getCmdArguments().add(destImageDimension);      
-      invoker.getCmdArguments().add("-extent");
+      invoker.getCmdArguments().add("-resize");
       invoker.getCmdArguments().add(destImageDimension);
       if (center)
       {
          invoker.getCmdArguments().add("-gravity");
-         invoker.getCmdArguments().add(destImageDimension);
+         invoker.getCmdArguments().add("Center");
       }
       invoker.getCmdArguments().add("-background");
       if (transparentBg)
@@ -41,8 +41,57 @@ public class ImageFileProcessingUtils
       {
          invoker.getCmdArguments().add("white");         
       }
+      invoker.getCmdArguments().add("-extent");
+      invoker.getCmdArguments().add(destImageDimension);
       invoker.getCmdArguments().add(destImgFile);
       
       invoker.execute();
+      
+      return invoker.getExitCode() == 0;
+   }
+   
+   public String getImageInfo(String imageFileName)
+   {
+      String retVal = null;
+      
+      String exeProgPath = getProgramPath(externalProgramsPath, "identify");
+
+      ProgramInvoker invoker
+         = new ProgramInvoker();
+      invoker.setCommandExec(exeProgPath);
+      invoker.getCmdArguments().add("-ping");
+      invoker.getCmdArguments().add("-format");
+      invoker.getCmdArguments().add("\"%m %w %h\"");
+      invoker.getCmdArguments().add(imageFileName);
+      
+      invoker.execute();
+      boolean opSuccess = invoker.getExitCode() == 0; 
+      if (opSuccess)
+      {
+         retVal = invoker.getCmdStdOut();
+      }
+      
+      return retVal;
+   }
+   
+   private static String getProgramPath(String programPath, String programName)
+   {
+      String fullProgramName;
+      if (isWindows())
+      {
+         fullProgramName = String.format("%s\\%s.exe", programPath, programName);
+      }
+      else
+      {
+         fullProgramName = String.format("%s/%s", programPath, programName);
+      }
+      
+      return fullProgramName;
+   }
+   
+   private static boolean isWindows()
+   {
+      String osName = System.getProperty("os.name");
+      return osName.contains("windows");
    }
 }
